aca503614c517d032f46a0c4bc7db2c8abf8cdb1
yimimrzhang <47467115+yimimrzhang@users.noreply.github.com>
2024-04-26 13:38:47 -0400

    Add files via upload

---
+++ hw7.R
@@ -0,0 +1,71 @@
+###################################
+####### Assignment Seven  #########
+###################################
+
+# parts a & b #
+library(tidyverse)
+dungaree <- read_csv('dungaree.csv', na=c(".", "NA", "", "?"))
+print(dungaree)
+
+
+# part c #
+library(Amelia)
+missmap(dungaree)
+dev.off()
+
+library(dlookr)
+plot_hist_numeric(dungaree)
+
+
+# part e #
+dungaree.scale<-dungaree %>% select(-c(STOREID, SALESTOT))
+
+library(caret)
+stdParams <- dungaree.scale %>% 
+              preProcess(method=c("center", "scale"))
+
+dungaree.scale<- predict(stdParams, dungaree.scale)
+
+
+
+# part f #
+set.seed(4321)
+km <- kmeans(x = dungaree.scale, centers = 6, nstart=25)
+table(km$cluster)  
+
+
+# part g #
+dungaree$cluster <-as.factor(km$cluster)
+
+
+# Density of Original jeans by cluster
+dungaree%>%
+  ggplot(aes(x=ORIGINAL)) +  
+  geom_density(lty=3) +      
+  geom_density(aes(fill=cluster, colour=cluster), alpha=0.5) 
+
+# Density of Fashion jeans by cluster
+dungaree%>%
+  ggplot(aes(x=FASHION)) +  
+  geom_density(lty=3) +      
+  geom_density(aes(fill=cluster, colour=cluster), alpha=0.5) 
+
+# Density of Leisure jeans by cluster
+dungaree%>%
+  ggplot(aes(x=LEISURE)) +  
+  geom_density(lty=3) +      
+  geom_density(aes(fill=cluster, colour=cluster), alpha=0.5) 
+
+# Density of Stretch jeans by cluster
+dungaree%>%
+  ggplot(aes(x=STRETCH)) +  
+  geom_density(lty=3) +      
+  geom_density(aes(fill=cluster, colour=cluster), alpha=0.5) 
+
+
+
+
+
+
+
+
